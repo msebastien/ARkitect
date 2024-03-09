2d2d3ca877955883333880e8313846246dd21d5a
Sébastien Maes <sebastien.maes@protonmail.com>
2024-03-09 22:04:00 +0100

    [UI] Implement Itembar slot and Drag icon

---
+++ Assets/Scripts/UI/Items/DragIcon.cs
@@ -0,0 +1,21 @@
+using ARKitect.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ARKitect.UI.Items
+{
+    public class DragIcon : Singleton<DragIcon>
+    {
+        private Image icon;
+
+        private void Awake()
+        {
+            icon = GetComponent<Image>();
+        }
+        
+        public void SetSprite(Sprite sprite) { icon.sprite = sprite; }
+    }
+
+}
--- Assets/Scripts/UI/Items/UIItembarSlot.cs
+++ Assets/Scripts/UI/Items/UIItembarSlot.cs
@@ -4,11 +4,114 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+
+using ARKitect.Items;
+using ARKitect.Core;
+using Logger = ARKitect.Core.Logger;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace ARKitect.UI.Items
 {
-    public class UIItembarSlot : MonoBehaviour
+    /// <summary>
+    /// Manage a item slot in shortcut bar
+    /// </summary>
+    public class UIItemBarSlot : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
     {
+        [SerializeField]
+        private Image icon;
+
+        private ItemsController itemBar;
+
+        public byte index;
+
+        public void Bind(ItemsController itemBar, byte index)
+        {
+            this.index = index;
+            this.itemBar = itemBar;
+            Identifier itemDefinitionId = itemBar.ItemDefinitionsInSlots.Count > index ? itemBar.ItemDefinitionsInSlots[index] : new Identifier();
+            ShowItemDefinition(itemDefinitionId);
+        }
+
+        public void RefreshItemVisuals()
+        {
+            Identifier itemDefinitionId = itemBar.ItemDefinitionsInSlots.Count > index ? itemBar.ItemDefinitionsInSlots[index] : new Identifier();
+            ShowItemDefinition(itemDefinitionId);
+        }
+
+        public void ShowItemDefinition(Identifier itemDefinitionId)
+        {
+            var itemDefinition = PrefabsManager.Instance.Items[itemDefinitionId];
+
+            if (itemDefinition == null) { Logger.LogError($"Item {itemDefinitionId} not found in catalog."); }
+
+            if (icon == null) { Logger.LogError("Icon is null"); return; }
+
+            icon.sprite = itemDefinition?.Icon;
+        }
+
+        public void OnBeginDrag(ExtendedPointerEventData eventData)
+        {
+            DragIcon.Instance.SetSprite(icon.sprite);
+            DragIcon.Instance.gameObject.SetActive(true);
+        }
+
+        public void OnDrag(ExtendedPointerEventData eventData)
+        {
+            DragIcon.Instance.transform.position = eventData.position;
+        }
+
+        public void OnEndDrag(ExtendedPointerEventData eventData)
+        {
+            if (DropOnItemSlot(eventData) ||
+                DropOnGround())
+            {
+                DragIcon.Instance.SetSprite(null);
+                DragIcon.Instance.gameObject.SetActive(false);
+            }
+        }
+
+        private bool DropOnGround()
+        {
+            var ray = UnityEngine.Camera.main.ScreenPointToRay(Touchscreen.current.primaryTouch.position.ReadValue());
+            if(Physics.Raycast(ray, out var hit)) 
+            {
+                // TODO: Spawn Item's prefab
+                itemBar.Spawn(hit.point);
+                return true;
+            }
+            return false;
+        }
+
+        private bool DropOnItemSlot(ExtendedPointerEventData eventData)
+        {
+            var hits = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, hits);
+
+            foreach(var hit in hits)
+            {
+                var droppedSlot = hit.gameObject.GetComponent<UIItemBarSlot>();
+                if(droppedSlot)
+                {
+                    Logger.LogInfo($"Drag End {droppedSlot?.name}");
+                    Swap(this, droppedSlot);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(UIItemBarSlot slot1, UIItemBarSlot slot2)
+        {
+            itemBar.Swap(slot1.index, slot2.index);
+        }
+        
+
         
     }
 
