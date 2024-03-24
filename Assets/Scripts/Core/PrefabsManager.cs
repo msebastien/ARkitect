d8d17115225dce22015649860033992fb8b21c99
Sébastien Maes <sebastien.maes@protonmail.com>
2024-03-24 23:11:14 +0100

    Add "Prefabs" Manager

---
+++ Assets/Scripts/Core/PrefabsManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+using ARKitect.Items;
+using Sirenix.OdinInspector;
+
+namespace ARKitect.Core
+{
+    /// <summary>
+    /// Manage prefab instances and store item definitions
+    /// </summary>
+    public class PrefabsManager : SerializedSingleton<PrefabsManager>
+    {
+        // Maybe a "resource locator" instead of a basic string like "namespace:itemID" ?
+        // Use OdinInspector to serialize the dictionary
+        [DictionaryDrawerSettings(KeyLabel = "Identifier", ValueLabel = "Item properties")]
+        [SerializeField]
+        private Dictionary<Identifier, Item> itemCatalog = new Dictionary<Identifier, Item>();
+        public Dictionary<Identifier, Item> Items => itemCatalog;
+        
+    }
+
+}
---
+++ Assets/Scripts/Core/SerializedSingleton.cs
@@ -0,0 +1,50 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+namespace ARKitect.Core
+{
+    /// <summary>
+    /// Make sure only one object instance is created and accessible
+    /// </summary>
+    /// <typeparam name="T">Object type</typeparam>
+    public class SerializedSingleton<T> : SerializedMonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                // If the instance has not initialized
+                if (_instance == null)
+                {
+                    var objs = FindObjectsOfType<T>();
+
+                    // If multiple instances of the same class have been found, keep the first one
+                    if (objs.Length > 0)
+                    {
+                        _instance = objs[0];
+                    }
+
+                    // Warn that multiple instances of the same singleton class exist, which SHOULD NOT happen
+                    if (objs.Length > 1)
+                    {
+                        Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                    }
+
+                    // Initialize a new instance
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = string.Format("_{0}", typeof(T).Name);
+                        obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+}
