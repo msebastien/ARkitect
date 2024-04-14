2308d71e609189c11a773640681279c17b7d588a
Sébastien Maes <sebastien.maes@protonmail.com>
2024-04-14 18:38:12 +0200

    UI: Minor fix for dragging and dropping items on nothing

--- Assets/Scripts/UI/Items/DragIcon.cs
+++ Assets/Scripts/UI/Items/DragIcon.cs
@@ -15,8 +15,22 @@ namespace ARKitect.UI.Items
             icon = GetComponent<Image>();
         }
 
-        public void SetSprite(Sprite sprite) { icon.sprite = sprite; }
-        public void SetColor(Color color) { icon.color = color; }
+        private void SetSprite(Sprite sprite) { icon.sprite = sprite; }
+        private void SetColor(Color color) { icon.color = color; }
+
+        public void SetIcon(Sprite icon)
+        {
+            SetSprite(icon);
+            SetColor(Color.white);
+            gameObject.SetActive(true);
+        }
+
+        public void Clear()
+        {
+            SetSprite(null);
+            SetColor(Color.clear);
+            gameObject.SetActive(false);
+        }
     }
 
 }
