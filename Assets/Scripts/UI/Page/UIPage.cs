0584f156d72416931167b29a36dca7c0ab01544f
Sébastien Maes <sebastien.maes@protonmail.com>
2024-04-17 02:20:43 +0200

    Add Page and Page Event Handler component

---
+++ Assets/Scripts/UI/Page/UIPage.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+using ARKitect.Coroutine;
+using Logger = ARKitect.Core.Logger;
+
+namespace ARKitect.UI.Page
+{
+    [AddComponentMenu("ARkitect/UI/Page/Page")]
+    public class UIPage : MonoBehaviour
+    {
+        [SerializeField]
+        private string _identifier;
+        public string Id 
+        { 
+            get => _identifier;
+        }
+
+        [SerializeField] 
+        private int _renderingOrder;
+
+        private CanvasGroup _canvasGroup;
+        private RectTransform _parentTransform;
+        private RectTransform _rectTransform;
+
+        public bool IsTransitioning { get; private set; }
+
+        /// <summary>
+        ///     Return the transition animation type currently playing.
+        ///     If not in transition, return PageTransitionAnimationType.None
+        /// </summary>
+        [SerializeField]
+        private PageTransitionAnimationType _transitionAnimationType;
+        public PageTransitionAnimationType TransitionAnimationType => _transitionAnimationType;
+
+        [SerializeField]
+        private UIAbstractPageTransitionAnimations _transitions;
+
+        private void Awake()
+        {
+            if(_transitions == null)
+                _transitions = gameObject.GetOrAddComponent<UIAbstractPageTransitionAnimations>();
+        }
+
+        internal void Init(RectTransform parentTransform)
+        {
+            _rectTransform = (RectTransform)transform;
+            _canvasGroup = gameObject.GetOrAddComponent<CanvasGroup>();
+            _parentTransform = parentTransform;
+
+            // Set order of rendering.
+            var siblingIndex = 0;
+            for (var i = 0; i < _parentTransform.childCount; i++)
+            {
+                var child = _parentTransform.GetChild(i);
+                var childPage = child.GetComponent<UIPage>();
+                siblingIndex = i;
+                if (_renderingOrder >= childPage._renderingOrder)
+                    continue;
+
+                break;
+            }
+
+            _rectTransform.SetSiblingIndex(siblingIndex);
+
+            _canvasGroup.alpha = 0.0f;
+        }
+
+        internal void BeforeEnter(bool push, UIPage partnerPage)
+        {
+            IsTransitioning = true;
+            _transitionAnimationType =
+                push ? PageTransitionAnimationType.PushEnter : PageTransitionAnimationType.PopEnter;
+            //gameObject.SetActive(true);
+            _canvasGroup.alpha = 1.0f;
+        }
+
+        internal AsyncProcessHandle Enter(UIPage partnerPage, bool playAnimation = true)
+        {
+            return CoroutineManager.Instance.Run(EnterRoutine(partnerPage, playAnimation));
+        }
+
+        private IEnumerator EnterRoutine(UIPage partnerPage, bool playAnimation)
+        {
+            if(playAnimation)
+            {
+                switch(_transitionAnimationType)
+                {
+                    case PageTransitionAnimationType.PushEnter:
+                        yield return _transitions.AnimatePushEnter(partnerPage);
+                        break;
+                    case PageTransitionAnimationType.PopEnter:
+                        yield return _transitions.AnimatePopEnter(partnerPage);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            _canvasGroup.alpha = 1.0f;
+        }
+
+        internal void AfterEnter(UIPage partnerPage)
+        {
+            IsTransitioning = false;
+            _transitionAnimationType = PageTransitionAnimationType.None;
+        }
+
+        internal void BeforeExit(bool push, UIPage partnerPage)
+        {
+            IsTransitioning = true;
+            _transitionAnimationType = push ? PageTransitionAnimationType.PushExit : PageTransitionAnimationType.PopExit;
+            //gameObject.SetActive(true);
+            _canvasGroup.alpha = 1.0f;
+        }
+
+        internal AsyncProcessHandle Exit(UIPage partnerPage, bool playAnimation = true)
+        {
+            return CoroutineManager.Instance.Run(ExitRoutine(partnerPage, playAnimation));
+        }
+
+        private IEnumerator ExitRoutine(UIPage partnerPage, bool playAnimation)
+        {
+            if(playAnimation)
+            {
+                switch (_transitionAnimationType)
+                {
+                    case PageTransitionAnimationType.PushExit:
+                        yield return _transitions.AnimatePushExit(partnerPage);
+                        break;
+                    case PageTransitionAnimationType.PopExit:
+                        yield return _transitions.AnimatePopExit(partnerPage);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            _canvasGroup.alpha = 0.0f;
+        }
+
+        internal void AfterExit(UIPage partnerPage)
+        {
+            //gameObject.SetActive(false);
+            IsTransitioning = false;
+            _transitionAnimationType = PageTransitionAnimationType.None;
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/Page/UIPageEventHandler.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace ARKitect.UI.Page
+{
+    /// <summary>
+    /// This script allows to easily push/pop pages by registering simple methods to UI elements' events, such as buttons with the OnClick() event. 
+    /// </summary>
+    [AddComponentMenu("ARkitect/UI/Page/Page Event Handler")]
+    public class UIPageEventHandler : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Reference to the page container managing all the pages")]
+        private UIPageContainer _container;
+
+        [SerializeField]
+        [Tooltip("The new page to push to display")]
+        private UIPage _page;
+
+        /// <summary>
+        /// Push the new page to display it
+        /// </summary>
+        public void Push()
+        {
+            if (_page != null && _container != null)
+            {
+                _container.Push(_page.Id);
+            }
+        }
+
+        /// <summary>
+        /// Pop the current active page to go back to the previous one
+        /// </summary>
+        public void Pop()
+        {
+            if (_container != null)
+            {
+                _container.Pop();
+            }
+        }
+    }
+
+}
