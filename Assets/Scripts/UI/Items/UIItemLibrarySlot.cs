9df83e54b01807723aa1cbf3cc935ee4afef3905
Sébastien Maes <sebastien.maes@protonmail.com>
2024-04-21 16:34:19 +0200

    ItemLibrarySlot: Derive from IPointerDownHandler

--- Assets/Scripts/UI/Items/UIItemLibrarySlot.cs
+++ Assets/Scripts/UI/Items/UIItemLibrarySlot.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 
 using ARKitect.Items;
+using Logger = ARKitect.Core.Logger;
+
 
 namespace ARKitect.UI.Items
 {
     /// <summary>
     /// Manage a item slot in library
     /// </summary>
-    public class UIItemLibrarySlot : UISlot
+    [AddComponentMenu("ARkitect/UI/Slots/Item library slot")]
+    public class UIItemLibrarySlot : UISlot, IPointerDownHandler
     {
-
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
 }
