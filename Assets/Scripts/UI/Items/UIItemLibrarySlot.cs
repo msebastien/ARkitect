c928a2f9428168520e2f9e0f9b1d62603dc36eb8
Sébastien Maes <sebastien.maes@protonmail.com>
2024-05-01 23:33:41 +0200

    UI/ItemLibrarySlot: Toggle favorite/star icon + override Refresh method

--- Assets/Scripts/UI/Items/UIItemLibrarySlot.cs
+++ Assets/Scripts/UI/Items/UIItemLibrarySlot.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+using ARKitect.Core;
 using ARKitect.UI.Modal;
+using ARKitect.UI.Colors;
 using Logger = ARKitect.Core.Logger;
-
 
 namespace ARKitect.UI.Items
 {
@@ -22,15 +24,43 @@ namespace ARKitect.UI.Items
             set => _modalId = value;
         }
 
+        [Header("Favorite")]
+        [SerializeField]
+        private Image _iconFavorite;
+        [SerializeField]
+        private UIColor _iconFavoriteColor;
+
+        private void Start()
+        {
+            _iconFavorite.color = Color.clear;
+        }
+
+        public override void RefreshItemVisuals()
+        {
+            base.RefreshItemVisuals();
+
+            if (_iconFavorite != null)
+            {
+                if (PrefabsManager.Items[controller.GetItemId(index)].MarkedAsFavorite)
+                    _iconFavorite.color = _iconFavoriteColor._color;
+                else
+                    _iconFavorite.color = Color.clear;
+            }
+        }
+
         public void OnPointerDown(PointerEventData eventData)
         {
             UIModalContainer.Instance.Push(_modalId, true, (modal) =>
             {
+                var itemId = controller.GetItemId(index);
+
                 var itemInfo = modal.gameObject.GetComponent<UIItemInfo>();
                 if (itemInfo != null)
-                {
-                    itemInfo.ItemId = controller.GetItemId(index).ToString();
-                }
+                    itemInfo.ItemId = itemId.ToString();
+
+                var itemSlotActions = modal.gameObject.GetComponent<UISlotActions>();
+                if (itemSlotActions != null)
+                    itemSlotActions.SetItemSlot(this, itemId);
             });
 
             Logger.LogInfo($"Item '{controller.GetItemId(index)}' clicked");
