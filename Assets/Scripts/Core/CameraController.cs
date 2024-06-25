696b76b32f24d34ece60453cd8cb1b9de385e2e4
Sébastien Maes <sebastien.maes@protonmail.com>
2024-06-25 21:20:23 +0200

    Camera Controller: tidy up imports

--- Assets/Scripts/Core/CameraController.cs
+++ Assets/Scripts/Core/CameraController.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
 
 using ARKitect.UI;
-using System;
 
 namespace ARKitect.Core
 {
