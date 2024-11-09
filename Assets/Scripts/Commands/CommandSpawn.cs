0779b96acf36d404129e2ef71fcfca2f5b0a2169
Sébastien Maes <sebastien.maes@protonmail.com>
2024-11-09 18:55:43 +0100

    CommandSpawn: Rename field, add log messages, override ToString() method

--- Assets/Scripts/Commands/CommandSpawn.cs
+++ Assets/Scripts/Commands/CommandSpawn.cs
@@ -8,32 +8,39 @@ namespace ARKitect.Commands
 {
     public class CommandSpawn : ICommand
     {
-        private IResourceObject _itemResource;
+        private IResourceObject _obj;
         private Vector3 _position;
         private Quaternion _rotation;
         private Guid _instanceID = Guid.Empty;
 
         public CommandSpawn(IResourceObject itemObject, Vector3 position, Quaternion rotation)
         {
-            _itemResource = itemObject;
+            _obj = itemObject;
             _position = position;
             _rotation = rotation;
+
+            Logger.LogWarning($"{ToString()}");
         }
 
         public void Execute()
         {
-            if(_instanceID == Guid.Empty)
-                _instanceID = _itemResource.Spawn(_position, _rotation);
+            if (_instanceID == Guid.Empty)
+                _instanceID = _obj.Spawn(_position, _rotation);
             else
-                _itemResource.Spawn(_instanceID, _position, _rotation);
+                _obj.Spawn(_instanceID, _position, _rotation);
 
-            Logger.LogInfo($"Execute {GetType()}: Spawn object of '{_itemResource.Item}'.");
+            Logger.LogInfo($"Execute {GetType()}: Spawn object of '{_obj.Item}'.");
         }
 
         public void Undo()
         {
-            if(_itemResource.DestroyObject(_instanceID))
-                Logger.LogInfo($"Undo {GetType()}: Destroy instance '{_instanceID}' of the object '{_itemResource.Item}'.");
+            if (_obj.DestroyObject(_instanceID))
+                Logger.LogInfo($"Undo {GetType()}: Destroy instance '{_instanceID}' of the object '{_obj.Item}'.");
+        }
+
+        public override string ToString()
+        {
+            return $"Spawn Object (obj: {_obj.Item}/pos: {_position}/rot: {_rotation}/guid: {_instanceID})";
         }
     }
 
