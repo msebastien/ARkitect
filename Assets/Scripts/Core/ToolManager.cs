fcdb2f8f03f07a1c67b63710ce4ebbc4a756ea2b
Sébastien Maes <sebastien.maes@protonmail.com>
2025-02-09 18:35:28 +0100

    Add empty files

---
+++ Assets/Scripts/Core/ToolManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ARKitect.Core
+{
+    public class ToolManager : MonoBehaviour
+    {
+        
+    }
+
+}
