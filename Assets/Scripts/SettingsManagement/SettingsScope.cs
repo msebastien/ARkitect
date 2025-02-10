28a231b8eb1bda343a611744258f7baf6ebc4e67
Sébastien Maes <sebastien.maes@protonmail.com>
2025-02-10 23:16:35 +0100

    Add Settings scope enum

---
+++ Assets/Scripts/SettingsManagement/SettingsScope.cs
@@ -0,0 +1,18 @@
+namespace ARKitect.SettingsManagement
+{
+    /// <summary>
+    /// The Scope determines where it appears in the UI.
+    /// </summary>
+    public enum SettingsScope
+    {
+        /// <summary>
+        /// App user preferences
+        /// </summary>
+        User,
+
+        /// <summary>
+        /// Project-specific settings
+        /// </summary>
+        Project
+    }
+}
